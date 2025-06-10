68262b1c2a01ae66db84ee36228a63d71f1541f4
CarlosBasilio0 <cbasiliorosario@gmail.com>
2025-06-09 23:31:13 -0500

    Registrar

--- siprel/prestamos/admin.py
+++ siprel/prestamos/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from .models import Usuario, Equipo, Prestamo, Historial, Notificacion, Categoria, Ubicacion, Mantenimiento, Sancion, Reporte
-
 # Registrar los modelos en el admin de Django
 admin.site.register(Usuario)
 admin.site.register(Equipo)
